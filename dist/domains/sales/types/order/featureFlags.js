4f527b2bc10a18d82467e08433842119f88c31c7
Mark D Armstrong <69395720+MarkArmsrong@users.noreply.github.com>
2025-03-14 14:00:28 -0600

    Removed dist and .vscode from tracking

--- dist/domains/sales/types/order/featureFlags.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.featureFlags = void 0;
-exports.featureFlags = {
-    loyalty: true,
-    seasonalPromo: false
-};
-//# sourceMappingURL=featureFlags.js.map
--- dist/domains/sales/values/featureFlags.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.featureFlags = void 0;
-exports.featureFlags = {
-    'loyalty': true, // This flag enables the LoyaltyDiscountStrategy
-    'seasonalPromo': true, // This flag disables the SeasonalPromoDiscountStrategy
-};
-//# sourceMappingURL=featureFlags.js.map
